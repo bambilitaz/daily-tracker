e42efe7ea1faa0adc840176346b0435310507065
Kwang <kwang@finema.co>
2024-09-16 18:32:12 +0700

    chore: add constant team

--- constants/team.ts
+++ constants/team.ts
@@ -1,11 +1,15 @@
 export const enum TEAM {
   DEV = 'dev',
   TESTER = 'tester',
+  UXUI = 'uxui',
+  BASA = 'basa',
 }
 
 export const TEAM_LABEL = {
   [TEAM.DEV]: 'Developer',
   [TEAM.TESTER]: 'Tester',
+  [TEAM.UXUI]: 'UX/UI',
+  [TEAM.BASA]: 'BA/SA',
 }
 
 export const TEAM_OPTIONS = [
@@ -16,5 +20,13 @@ export const TEAM_OPTIONS = [
   {
     label: TEAM_LABEL[TEAM.TESTER],
     value: TEAM.TESTER,
+  },
+  {
+    label: TEAM_LABEL[TEAM.UXUI],
+    value: TEAM.UXUI,
+  },
+  {
+    label: TEAM_LABEL[TEAM.BASA],
+    value: TEAM.BASA,
   },
 ]
